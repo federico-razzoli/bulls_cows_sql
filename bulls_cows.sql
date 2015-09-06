5231d1a6f94cce91e14e4a04c2bd4207a1bece0b
Federico Razzoli <federico@ibuildings.it>
2015-09-06 20:47:12 +0200

    @digit_number
    fix trivial bugs

--- bulls_cows.sql
+++ bulls_cows.sql
@@ -34,7 +34,12 @@ CREATE PROCEDURE new()
         COMMENT 'Start new game'
 BEGIN
         DECLARE v_cur_digit TINYINT UNSIGNED DEFAULT NULL;
-        DECLARE i TINYINT UNSIGNED DEFAULT 3;
+        DECLARE i TINYINT UNSIGNED DEFAULT @digit_number - 1;
+        
+        -- default number of digits is 4
+        IF @digit_number IS NULL THEN
+                SET @digit_number := 4;
+        END IF;
         
         CREATE OR REPLACE TEMPORARY TABLE digit
                 ENGINE = MEMORY
@@ -59,7 +64,7 @@ END ||
 
 
 DROP PROCEDURE IF EXISTS guess;
-CREATE PROCEDURE guess(IN p_number CHAR(4))
+CREATE PROCEDURE guess(IN p_number VARCHAR(100))
         MODIFIES SQL DATA
         COMMENT 'Guess secret number'
 BEGIN
@@ -69,8 +74,10 @@ BEGIN
         SET @bulls  := 0;
         SET @cows   := 0;
         
-        IF p_number IS NULL OR p_number NOT REGEXP '[[:digit:]]{4}' THEN
-                SET error_message := 'Please enter 4 digits';
+        SET p_number := REPLACE(p_number, ' ', '');
+        
+        IF p_number IS NULL OR p_number NOT REGEXP CONCAT('^[[:digit:]]{', @digit_number, '}$') THEN
+                SET error_message := CONCAT('Please enter ', @digit_number, ' digits');
         ELSEIF @allow_twin_cows IS NULL OR @allow_twin_cows IS FALSE THEN
                 CREATE OR REPLACE TEMPORARY TABLE last_digits
                 (
@@ -78,12 +85,18 @@ BEGIN
                 )
                         ENGINE = MEMORY
                 ;
-                INSERT IGNORE INTO last_digits SELECT SUBSTRING(p_number FROM 1 FOR 1);
-                INSERT IGNORE INTO last_digits SELECT SUBSTRING(p_number FROM 2 FOR 1);
-                INSERT IGNORE INTO last_digits SELECT SUBSTRING(p_number FROM 3 FOR 1);
-                INSERT IGNORE INTO last_digits SELECT SUBSTRING(p_number FROM 4 FOR 1);
                 
-                IF (SELECT COUNT(DISTINCT n) FROM last_digits) <> 4 THEN
+                BEGIN
+                        DECLARE i TINYINT UNSIGNED DEFAULT NULL;
+                        SET i := @digit_number;
+                        SET i := @digit_number;
+                        WHILE i > 0 DO
+                                INSERT IGNORE INTO last_digits SELECT SUBSTRING(p_number FROM i FOR 1);
+                                SET i := i - 1;
+                        END WHILE;
+                END;
+                
+                IF (SELECT COUNT(DISTINCT n) FROM last_digits) <> @digit_number THEN
                         SET error_message := 'Twin cows not allowed!\nAll digits must be different';
                 END IF;
                 
@@ -97,7 +110,7 @@ BEGIN
                         DECLARE secret_i, guess_i TINYINT UNSIGNED DEFAULT 1;
                         DECLARE secret_cur, guess_cur CHAR(1) DEFAULT NULL;
                         
-                        WHILE secret_i < 5 DO
+                        WHILE secret_i <= @digit_number DO
                                 SET secret_cur  := SUBSTRING(@digits FROM secret_i FOR 1);
                                 SET guess_cur   := SUBSTRING(p_number FROM secret_i FOR 1);
                                 
@@ -106,7 +119,7 @@ BEGIN
                                 ELSE
                                         SET guess_i := 1;
                                         
-                                        WHILE guess_i < 5 DO
+                                        WHILE guess_i <= @digit_number DO
                                                 SET guess_cur := SUBSTRING(p_number FROM guess_i FOR 1);
                                                 
                                                 IF guess_cur = secret_cur THEN
@@ -121,8 +134,8 @@ BEGIN
                         END WHILE;
                 END;
                 
-                IF @bulls = 4 THEN
-                        SELECT '\n4 BULLZ!\n\nCongratulations, pal!\n' AS `Message`;
+                IF @bulls = @digit_number THEN
+                        SELECT CONCAT('\n', @digit_number, ' BULLZ!\n\nCongratulations, pal!\n') AS `Message`;
                 ELSEIF @bulls = 0 AND @cows = 0 THEN
                         SELECT '\n0 bulls, 0 cows...\n\nHow sad. Try again!\n' AS `Message`;
                 ELSE
@@ -150,9 +163,13 @@ BEGIN
                         , '1 cow, because 4 is correct, but in the wrong position.\n'
                         , '\n'
                         , 'To make me think a new number, type:\n'
-                        , 'CALL new();\n', '\n'
+                        , 'CALL new();\n'
+                        , '\n'
                         , 'To try guessing the secret number, type:\n'
                         , 'CALL guess(''1234'');\n'
+                        , '\n'
+                        , 'To change the number of digits:\n'
+                        , 'SET @digit_number := 3;\n'
                         , '\n'
                         , 'You don''t want to call new() each time\n'
                         , 'if you want to retry, or if you''re playing with someone else.\n'
